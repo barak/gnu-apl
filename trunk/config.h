290c6873b34d519a33cf2528229f0bd0f1f14e71
j_sauermann <bug-apl@gnu.org>
2022-11-25 18:13:16 +0100

    sync from SVN

--- trunk/config.h
+++ trunk/config.h
@@ -5,7 +5,7 @@
 #define ARCHIVE_SVN " 19548"
 
 /* How and when GNU APL was configured */
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 19668", "2022-11-25 16:57:44 UTC", "Linux 4.15.0-158-generic x86_64", "default ./configure options"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 19670", "2022-11-25 17:07:38 UTC", "Linux 4.15.0-158-generic x86_64", "default ./configure options"
 
 /* Define to 1 iff pthread_setaffinity_np() is available */
 #define HAVE_AFFINITY_NP 1
