11156c109d331ac74170bc7c4585a5148c54e425
eedjsa <eedjsa@server68>
2023-10-26 16:22:00 +0200

    sync from SVN

--- trunk/config.h
+++ trunk/config.h
@@ -5,7 +5,7 @@
 #define ARCHIVE_SVN " 883"
 
 /* How and when GNU APL was configured */
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 1145", "2023-10-26 14:05:08 UTC", "Linux 5.15.0-86-generic x86_64", "default ./configure options"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 1146", "2023-10-26 14:16:18 UTC", "Linux 5.15.0-86-generic x86_64", "default ./configure options"
 
 /* Define to 1 iff pthread_setaffinity_np() is available */
 #define HAVE_AFFINITY_NP 1
